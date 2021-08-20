b681d4e9973c8a2bf13576b1fb8bc008ea65c646
mkkieru <mkieru55@gmail.com>
2021-08-20 15:41:29 +0300

    Add changes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,7 +12,15 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-    public static void main(String[] args) { //type “psvm + tab” to autocreate this
+    static int getHerokuAssignedPort() {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (processBuilder.environment().get("PORT") != null) {
+        return Integer.parseInt(processBuilder.environment().get("PORT"));
+    }
+    return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+}
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
         String connectionString = "jdbc:postgresql://localhost:5432/wildlife_tracker";
         Sql2o sql2o = new Sql2o(connectionString, "damark", "password");
