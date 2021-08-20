6e7fcbc23bdd4e38de2b08afb006f7e5ed1aae91
mkkieru <mkieru55@gmail.com>
2021-08-20 12:08:13 +0300

    Add new Location Tests

--- src/test/java/models/locationTest.java
+++ src/test/java/models/locationTest.java
@@ -6,14 +6,26 @@ import org.junit.Test;
 public class locationTest {
 
     @Test
-    public void newLocation(){
+    public void newLocationInstantiatesCorrectly(){
         Location newLocation = new Location("Near the river");
         Assert.assertTrue(newLocation instanceof Location);
+    }
+    @Test
+    public void TestsetLocation(){
+        Location newLocation = new Location("");
+        newLocation.setLocation("Near the river");
+        Assert.assertEquals("Near the river", newLocation.getLocation());
     }
     @Test
     public void getLocation(){
         Location newLocation = new Location("Near the river");
         Assert.assertEquals("Near the river", newLocation.getLocation());
+    }
+    @Test
+    public void TestsetIdAndGetId(){
+        Location newLocation = new Location("Near the river");
+        newLocation.setId(2);
+        Assert.assertTrue(2 == newLocation.getId());
     }
 
 }
