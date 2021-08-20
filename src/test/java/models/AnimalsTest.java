7b0ebdcfd00caab39de19e21acb2386ab5224568
mkkieru <mkieru55@gmail.com>
2021-08-20 12:07:54 +0300

    Add new AnimalsTests

--- src/test/java/models/AnimalsTest.java
+++ src/test/java/models/AnimalsTest.java
@@ -13,6 +13,42 @@ public class AnimalsTest {
     }
 
     @Test
+    public void setNameSetsCorrectly(){
+        Animals newAnimal = new Animals("Mark","Human Being","2");
+        newAnimal.setSpecies("mark");
+        Assert.assertEquals("mark", newAnimal.getSpecies());
+    }
+    @Test
+    public void setSpeciesSetsCorrectly(){
+        Animals newAnimal = new Animals("Mark","Human Being","2");
+        newAnimal.setSpecies("human");
+        Assert.assertEquals("human", newAnimal.getSpecies());
+    }
+    @Test
+    public void setNameLocationIdCorrectly(){
+        Animals newAnimal = new Animals("Mark","Human Being","2");
+        newAnimal.setLocationId("2");
+        Assert.assertEquals("2", newAnimal.getLocationId());
+    }
+    @Test
+    public void setAgeSetsCorrectly(){
+        Animals newAnimal = new Animals("Mark","Human Being","2");
+        newAnimal.setAge("child");
+        Assert.assertEquals("child", newAnimal.getAge());
+    }
+    @Test
+    public void sethealthSetsCorrectly(){
+        Animals newAnimal = new Animals("Mark","Human Being","2");
+        newAnimal.setHealth("sick");
+        Assert.assertEquals("sick", newAnimal.getHealth());
+    }
+    @Test
+    public void setStatusSetsCorrectly(){
+        Animals newAnimal = new Animals("Mark","Human Being","2");
+        newAnimal.setStatus("endangered");
+        Assert.assertEquals("endangered", newAnimal.getStatus());
+    }
+    @Test
     public void getName(){
         Animals newAnimal = new Animals("Mark","Human Being","2");
         Assert.assertEquals("Mark", newAnimal.getName());
